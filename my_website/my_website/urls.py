5690ef568c09fb87f581da9ed3bcb3772d418224
Emin <makhmudovemin441@gmail.com>
2025-10-24 17:44:15 +0500

    added goods app connected catalog and product html

--- my_website/my_website/urls.py
+++ my_website/my_website/urls.py
@@ -20,5 +20,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
+
+"www.site.com/admin"
+"www.site.com"
+"www.site.com/about"
+"www.site.com/catalog"
+"www.site.com/catalog/product"
