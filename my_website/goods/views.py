dfb995de47983fe1e0159223ee44623ae5807094
Emin <makhmudovemin441@gmail.com>
2025-11-01 15:18:20 +0500

    second pagination option - GET params

--- my_website/goods/views.py
+++ my_website/goods/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from goods.models import Products
 # Create your views here.
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    
+    page = request.GET.get('page', 1)
     
     if category_slug == 'all':
         goods = Products.objects.all()
